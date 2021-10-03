4973baf4d7908e16adb721fd126beec744df5c43
JamesTang <jamestanggood@gmail.com>
2021-10-02 22:13:06 -0500

    Update lesson 01 and 02.

--- 01 Supervised Learning with scikit-learn/01-Classification.py
+++ 01 Supervised Learning with scikit-learn/01-Classification.py
@@ -1,4 +1,3 @@
-
 # 01 Classification
 '''
 In this chapter, you will be introduced to classification problems and learn how to solve them using supervised learning techniques. And you’ll apply what you learn to a political dataset, where you classify the party affiliation of United States congressmen based on their voting records.
