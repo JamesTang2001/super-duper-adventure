8ccc685179e53ccabd29afd06240708f1c5109cd
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-16 11:34:28 -0500

    Create 02-interacting-with-APIs-to-import-data-from-the-web.py

---
+++ 02-importing-&-cleaning-data-with-python/02-intermediate-importing-data-in-python/02-interacting-with-APIs-to-import-data-from-the-web.py
@@ -0,0 +1,11 @@
+# 02 Interacting with APIs to import data from the web
+'''
+In this chapter, you will gain a deeper understanding of how to import data from the web. You will learn the basics of extracting data from APIs, gain insight on the importance of APIs, and practice extracting data by diving into the OMDB and Library of Congress APIs.
+'''
+
+
+
+# Introduction to APIs and JSONs
+'''
+Video
+'''
