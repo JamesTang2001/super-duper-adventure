d27cdbffd6735d96aeb64c166f7e9324ed4844bf
JamesTang <jamestanggood@gmail.com>
2021-10-16 11:30:31 -0500

    Complete importing-data-from-the-internet

--- 01-machine-learning-fundamentals-with-python/01-supervised-learning-with-scikit-learn/04-preprocessing-and-pipelines.py
+++ 01-machine-learning-fundamentals-with-python/01-supervised-learning-with-scikit-learn/04-preprocessing-and-pipelines.py
@@ -9,3 +9,88 @@ This chapter introduces pipelines, and how scikit-learn allows for transformers 
 '''
 Video
 '''
+
+
+
+# Exploring categorical features
+'''
+The Gapminder dataset that you worked with in previous chapters also contained a categorical 'Region' feature, which we dropped in previous exercises since you did not have the tools to deal with it. Now however, you do, so we have added it back in!
+Your job in this exercise is to explore this feature. Boxplots are particularly useful for visualizing categorical features such as this.
+
+Instructions
+-Import pandas as pd.
+-Read the CSV file 'gapminder.csv' into a DataFrame called df.
+-Use pandas to create a boxplot showing the variation of life expectancy ('life') by region ('Region'). To do so, pass the column names in to df.boxplot() (in that order).
+'''
+# Import pandas
+import pandas as pd
+
+# Read 'gapminder.csv' into a DataFrame: df
+df = pd.read_csv("gapminder.csv")
+
+# Create a boxplot of life expectancy per region
+df.boxplot("life", "Region", rot=60)
+
+# Show the plot
+plt.show()
+
+
+
+# Creating dummy variables
+'''
+As Andy discussed in the video, scikit-learn does not accept non-numerical features. You saw in the previous exercise that the 'Region' feature contains very useful information that can predict life expectancy. For example, Sub-Saharan Africa has a lower life expectancy compared to Europe and Central Asia. Therefore, if you are trying to predict life expectancy, it would be preferable to retain the 'Region' feature. To do this, you need to binarize it by creating dummy variables, which is what you will do in this exercise.
+
+Instructions
+-Use the pandas get_dummies() function to create dummy variables from the df DataFrame. Store the result as df_region.
+-Print the columns of df_region. This has been done for you.
+-Use the get_dummies() function again, this time specifying drop_first=True to drop the unneeded dummy variable (in this case, 'Region_America').
+-Hit 'Submit Answer to print the new columns of df_region and take note of how one column was dropped!
+'''
+# Create dummy variables: df_region
+df_region = pd.get_dummies(df)
+
+# Print the columns of df_region
+print(df_region.columns)
+
+# Create dummy variables with drop_first=True: df_region
+df_region = pd.get_dummies(df, drop_first=True)
+
+# Print the new columns of df_region
+print(df_region.columns)
+
+
+
+# Regression with categorical features
+'''
+Having created the dummy variables from the 'Region' feature, you can build regression models as you did before. Here, you'll use ridge regression to perform 5-fold cross-validation.
+The feature array X and target variable array y have been pre-loaded.
+
+Instructions
+-Import Ridge from sklearn.linear_model and cross_val_score from sklearn.model_selection.
+-Instantiate a ridge regressor called ridge with alpha=0.5 and normalize=True.
+-Perform 5-fold cross-validation on X and y using the cross_val_score() function.
+-Print the cross-validated scores.
+'''
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+
+# Instantiate a ridge regressor: ridge
+ridge = Ridge(alpha=0.5, normalize=True)
+
+# Perform 5-fold cross-validation: ridge_cv
+ridge_cv = cross_val_score(ridge, X, y, cv=5)
+
+# Print the cross-validated scores
+print(ridge_cv)
+
+
+
+# Handling missing data
+'''
+Video
+'''
+
+
+
+# 
--- 02-importing-&-cleaning-data-with-python/02-intermediate-importing-data-in-python/01-importing-data-from-the-internet.py
+++ 02-importing-&-cleaning-data-with-python/02-intermediate-importing-data-in-python/01-importing-data-from-the-internet.py
@@ -214,4 +214,115 @@ Video
 
 
 
-# 
+# Parsing HTML with BeautifulSoup
+'''
+In this interactive exercise, you'll learn how to use the BeautifulSoup package to parse, prettify and extract information from HTML. You'll scrape the data from the webpage of Guido van Rossum, Python's very own Benevolent Dictator for Life. In the following exercises, you'll prettify the HTML and then extract the text and the hyperlinks.
+The URL of interest is url = 'https://www.python.org/~guido/'.
+
+Instructions
+-Import the function BeautifulSoup from the package bs4.
+-Assign the URL of interest to the variable url.
+-Package the request to the URL, send the request and catch the response with a single function requests.get(), assigning the response to the variable r.
+-Use the text attribute of the object r to return the HTML of the webpage as a string; store the result in a variable html_doc.
+-Create a BeautifulSoup object soup from the resulting HTML using the function BeautifulSoup().
+-Use the method prettify() on soup and assign the result to pretty_soup.
+-Hit submit to print to prettified HTML to your shell!
+'''
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = "https://www.python.org/~guido/"
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Prettify the BeautifulSoup object: pretty_soup
+pretty_soup = soup.prettify()
+
+# Print the response
+print(pretty_soup)
+
+
+
+# Turning a webpage into data using BeautifulSoup: getting the text
+'''
+As promised, in the following exercises, you'll learn the basics of extracting information from HTML soup. In this exercise, you'll figure out how to extract the text from the BDFL's webpage, along with printing the webpage's title.
+
+Instructions
+-In the sample code, the HTML response object html_doc has already been created: your first task is to Soupify it using the function BeautifulSoup() and to assign the resulting soup to the variable soup.
+-Extract the title from the HTML soup soup using the attribute title and assign the result to guido_title.
+-rint the title of Guido's webpage to the shell using the print() function.
+-Extract the text from the HTML soup soup using the method get_text() and assign to guido_text.
+-Hit submit to print the text from Guido's webpage to the shell.
+'''
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Get the title of Guido's webpage: guido_title
+guido_title = soup.title
+
+# Print the title of Guido's webpage to the shell
+print(guido_title)
+
+# Get Guido's text: guido_text
+guido_text = soup.get_text()
+
+# Print Guido's text to the shell
+print(guido_text)
+
+
+
+# Turning a webpage into data using BeautifulSoup: getting the hyperlinks
+'''
+In this exercise, you'll figure out how to extract the URLs of the hyperlinks from the BDFL's webpage. In the process, you'll become close friends with the soup method find_all().
+
+Instructions
+-Use the method find_all() to find all hyperlinks in soup, remembering that hyperlinks are defined by the HTML tag <a> but passed to find_all() without angle brackets; store the result in the variable a_tags.
+-The variable a_tags is a results set: your job now is to enumerate over it, using a for loop and to print the actual URLs of the hyperlinks; to do this, for every element link in a_tags, you want to print() link.get('href').
+'''
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+
+# create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Print the title of Guido's webpage
+print(soup.title)
+
+# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags = soup.find_all("a")
+
+# Print the URLs to the shell
+for link in a_tags:
+    print(link.get("href"))
