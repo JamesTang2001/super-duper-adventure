c3bf7dd922a0d349ec6a5e9afb3c532fa5f0087e
JamesTang <jamestanggood@gmail.com>
2021-10-16 10:40:53 -0500

    Complete fine-tuning-your-model

--- 02-importing-&-cleaning-data-with-python/02-intermediate-importing-data-in-python/01-importing-data-from-the-internet.py
+++ 02-importing-&-cleaning-data-with-python/02-intermediate-importing-data-in-python/01-importing-data-from-the-internet.py
@@ -155,4 +155,63 @@ Instructions
 -Print the string html.
 -Hit submit to perform all of the above and to close the response: be tidy!
 '''
-# Just started. Haven't complete the question.
+# Import packages
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+
+# This packages the request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Extract the response: html
+html = response.read()
+
+# Print the html
+print(html)
+
+# Be polite and close the response!
+response.close()
+
+
+
+# Performing HTTP requests in Python using requests
+'''
+Now that you've got your head and hands around making HTTP requests using the urllib package, you're going to figure out how to do the same using the higher-level requests library. You'll once again be pinging DataCamp servers for their "http://www.datacamp.com/teach/documentation" page.
+Note that unlike in the previous exercises using urllib, you don't have to close the connection when using requests!
+
+Instructions
+-Import the package requests.
+-Assign the URL of interest to the variable url.
+-Package the request to the URL, send the request and catch the response with a single function requests.get(), assigning the response to the variable r.
+-Use the text attribute of the object r to return the HTML of the webpage as a string; store the result in a variable text.
+-Hit submit to print the HTML of the webpage.
+'''
+# Import package
+import requests
+
+# Specify the url: url
+url = "http://www.datacamp.com/teach/documentation"
+
+# Packages the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response: text
+text = r.text
+
+# Print the html
+print(text)
+
+
+
+# Scraping the web in Python
+'''
+Video
+'''
+
+
+
+# 
