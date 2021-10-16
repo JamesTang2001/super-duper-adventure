b73429f02700bf44bca98435534d062287129bef
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-16 13:40:59 -0500

    Create 03-diving-deep-into-the-Twitter-API.py

---
+++ 02-importing-&-cleaning-data-with-python/02-intermediate-importing-data-in-python/03-diving-deep-into-the-Twitter-API.py
@@ -0,0 +1,15 @@
+# 03 Diving deep into the Twitter API
+'''
+In this chapter, you will consolidate your knowledge of interacting with APIs in a deep dive into the Twitter streaming API. You'll learn how to stream real-time Twitter data, and how to analyze and visualize it.
+'''
+
+
+
+# The Twitter API and Authentication
+'''
+Video
+'''
+
+
+
+# 
