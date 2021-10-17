091b2265b157fa0b2a87588622844a805e284284
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-17 13:35:23 -0500

    Create 01-welcome-to-python.py

---
+++ 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/01-welcome-to-python.py
@@ -0,0 +1,15 @@
+# 01 Welcome to Python
+'''
+This chapter is an introduction to basics in Python, including how to name variables and various data types in Python.
+'''
+
+
+
+# Welcome to Python for Finance!
+'''
+Video
+'''
+
+
+
+#
