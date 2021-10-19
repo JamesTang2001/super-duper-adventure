fdc6aedc706f35854668d80681f0f0058783eead
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-19 14:07:16 -0500

    Create 02-lists.py

---
+++ 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/02-lists.py
@@ -0,0 +1,18 @@
+# 02 Lists
+'''
+This chapter introduces lists in Python and how they can be used to work with data.
+'''
+
+
+
+# Lists
+'''
+Video
+'''
+
+
+
+# Creating lists in Python
+'''
+
+'''
