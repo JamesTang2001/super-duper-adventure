495e12ca4f99225b1d8dcc5a41af8cbe875204ef
JamesTang <jamestanggood@gmail.com>
2021-10-19 15:08:34 -0500

    Hope it works.

--- 01-machine-learning-fundamentals-with-python/02-unsupervised-learning-in-python/03-decorrelating-your-data-and-dimension-reduction.py
+++ 01-machine-learning-fundamentals-with-python/02-unsupervised-learning-in-python/03-decorrelating-your-data-and-dimension-reduction.py
@@ -49,5 +49,65 @@ print(correlation)
 
 # Decorrelating the grain measurements with PCA
 '''
+You observed in the previous exercise that the width and length measurements of the grain are correlated. Now, you'll use PCA to decorrelate these measurements, then plot the decorrelated points and measure their Pearson correlation.
+
+Instructions
+-Import PCA from sklearn.decomposition.
+-Create an instance of PCA called model.
+-Use the .fit_transform() method of model to apply the PCA transformation to grains. Assign the result to pca_features.
+-The subsequent code to extract, plot, and compute the Pearson correlation of the first two columns pca_features has been written for you, so hit 'Submit Answer' to see the result!
+'''
+# Import PCA
+from sklearn.decomposition import PCA
+
+# Create PCA instance: model
+model = PCA()
+
+# Apply the fit_transform method of model to grains: pca_features
+pca_features = model.fit_transform(grains)
+
+# Assign 0th column of pca_features: xs
+xs = pca_features[:,0]
+
+# Assign 1st column of pca_features: ys
+ys = pca_features[:,1]
+
+# Scatter plot xs vs ys
+plt.scatter(xs, ys)
+plt.axis('equal')
+plt.show()
+
+# Calculate the Pearson correlation of xs and ys
+correlation, pvalue = pearsonr(xs, ys)
+
+# Display the correlation
+print(correlation)
+
+
+
+# Principal components
+'''
+On the right are three scatter plots of the same point cloud. Each scatter plot shows a different set of axes (in red). In which of the plots could the axes represent the principal components of the point cloud?
+Recall that the principal components are the directions along which the the data varies.
+
+Possible Answers
+-None of them.
+-Both plot 1 and plot 3.
+-Plot 2
+
+# Both plot 1 and plot 3.
+'''
+
+
+
+# Intrinsic dimension
+'''
+Video
+'''
+
+
+
+# The first principal component
+'''
 
 '''
--- 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/02-lists.py
+++ 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/02-lists.py
@@ -14,5 +14,90 @@ Video
 
 # Creating lists in Python
 '''
+A list in Python can contain any number of elements. You generally create a list using square brackets. For example, to create a list of integers, you can use the following command:
+x = [1, 2, 3]
+
+Instructions
+-Create a list named names containing the strings 'Apple Inc', 'Coca-Cola', and 'Walmart', in that order.
+-Print names.
+-Create a list named prices containing the price of each stock, $159.54, $37.13, and $71.17, in that order.
+-Print prices.
+'''
+# Create and print list names
+names =["Apple Inc", "Coca-Cola", "Walmart"]
+print(names)
+
+# Create and print list prices
+prices = [159.54, 37.13, 71.17]
+print(prices)
+
+
+
+# Indexing list items
+'''
+Each item in a list has an assigned indexed value. Remember that Python is a zero indexed language, and the first element in a list is stored at index zero. In this exercise, you will practice subsetting single elements from a list.
+Lists names and prices for company names and stock prices are available in your workspace.
+
+Instructions
+-Print the first element in names.
+-Print the second element in names.
+-Using a negative index, print the last element in prices.
+'''
+# Print the first item in names
+print(names[0])
+
+# Print the second item in names
+print(names[1])
+
+# Print the last element in prices
+print(prices[-1])
+
+
+
+# 
+'''
+Slicing operations on a list are used to subset multiple elements from a list. The syntax for list slicing is as follows:
+list[start:end]
+
+Remember, this syntax indicates subsetting all elements from the start and up to but not including the end element.
+Also, you can use extended slicing to efficiently select multiple elements from a list. For example, the following command returns all elements from the list except the first (at index 0):
+x = [1, 2, 3, 4, 5]
+x[1:]
+[2, 3, 4, 5]
+
+Similarly, the following command returns all elements from the list except the last two:
+x[:-2]
+[1, 2, 3]
+
+Instructions 1/2
+-Use list slicing to subset the last two elements from names.
+Instructions 2/2
+-Use extended slicing and specifying appropriate indices to subset the first two elements from prices.
+'''
+# names
+names = ['Apple Inc', 'Coca-Cola', 'Walmart']
+
+# Use slicing on list names
+names_subset = names[-1:-3]
+print(names_subset)
+
+# prices
+prices = [159.54, 37.13, 71.17]
+
+# Use extended slicing on the list prices
+prices_subset = prices[:3]
+print(prices_subset)
+
+
+
+# Nested lists
+'''
+Video
+'''
+
+
+
+# Stock up a nested list
+'''
 
 '''
