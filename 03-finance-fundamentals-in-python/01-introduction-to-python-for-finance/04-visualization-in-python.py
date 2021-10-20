c6fe6be349a28644ffcf0cc6464dc0aa642d1e9d
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-20 00:07:44 -0500

    Create 04-visualization-in-python.py

---
+++ 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/04-visualization-in-python.py
@@ -0,0 +1,18 @@
+# 04 Visualization in Python
+'''
+In this chapter, you will be introduced to the Matplotlib package for creating line plots, scatter plots, and histograms.
+'''
+
+
+
+# Visualization in Python
+'''
+Video
+'''
+
+
+
+# Importing matplotlib and pyplot
+'''
+
+'''
