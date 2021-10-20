ae296ffca60e5496eaf6b13a4f67391b7e3f6fa8
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-20 14:32:17 -0500

    Create 05-s&p-100-case-study.py

---
+++ 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/05-s&p-100-case-study.py
@@ -0,0 +1,15 @@
+# 05 S&P 100 Case Study
+'''
+In this chapter, you will get a chance to apply all the techniques you learned in the course on the S&P 100 data.
+'''
+
+
+
+# Introducing the dataset
+'''
+Video
+'''
+
+
+
+# 
