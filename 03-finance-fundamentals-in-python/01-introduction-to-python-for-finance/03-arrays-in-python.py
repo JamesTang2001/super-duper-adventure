97cb338dfc4cf46516de5e4ef6ac5d95fd9da99d
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-19 22:19:33 -0500

    Create 03-arrays-in-python.py

---
+++ 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/03-arrays-in-python.py
@@ -0,0 +1,18 @@
+# 03 Arrays in Python
+'''
+This chapter introduces packages in Python, specifically the NumPy package and how it can be efficiently used to manipulate arrays.
+'''
+
+
+
+# Arrays
+'''
+Video
+'''
+
+
+
+# Create an array
+'''
+
+'''
