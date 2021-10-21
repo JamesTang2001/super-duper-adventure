1a2c75768f435b097f6122b6c39cc845350dc5fa
JamesTang <jamestanggood@gmail.com>
2021-10-21 18:33:11 -0500

    Complete course 01 Introduction to Python for Finance.

--- 01-machine-learning-fundamentals-with-python/02-unsupervised-learning-in-python/04-discovering-interpretable-features.py
+++ 01-machine-learning-fundamentals-with-python/02-unsupervised-learning-in-python/04-discovering-interpretable-features.py
@@ -100,3 +100,194 @@ Possible Answers
 '''
 Video
 '''
+
+
+
+# NMF learns topics of documents
+'''
+In the video, you learned when NMF is applied to documents, the components correspond to topics of documents, and the NMF features reconstruct the documents from the topics. Verify this for yourself for the NMF model that you built earlier using the Wikipedia articles. Previously, you saw that the 3rd NMF feature value was high for the articles about actors Anne Hathaway and Denzel Washington. In this exercise, identify the topic of the corresponding NMF component.
+The NMF model you built earlier is available as model, while words is a list of the words that label the columns of the word-frequency array.
+After you are done, take a moment to recognise the topic that the articles about Anne Hathaway and Denzel Washington have in common!
+
+Instructions
+-Import pandas as pd.
+-Create a DataFrame components_df from model.components_, setting columns=words so that columns are labeled by the words.
+-Print components_df.shape to check the dimensions of the DataFrame.
+-Use the .iloc[] accessor on the DataFrame components_df to select row 3. Assign the result to component.
+-Call the .nlargest() method of component, and print the result. This gives the five words with the highest values for that component.
+'''
+# Import pandas
+import pandas as pd
+
+# Create a DataFrame: components_df
+components_df = pd.DataFrame(model.components_, columns=words)
+
+# Print the shape of the DataFrame
+print(components_df.shape)
+
+# Select row 3: component
+component = components_df.iloc[3,:]
+
+# Print result of nlargest
+print(component.nlargest())
+
+
+
+# Explore the LED digits dataset
+'''
+In the following exercises, you'll use NMF to decompose grayscale images into their commonly occurring patterns. Firstly, explore the image dataset and see how it is encoded as an array. You are given 100 images as a 2D array samples, where each row represents a single 13x8 image. The images in your dataset are pictures of a LED digital display.
+
+Instructions
+-Import matplotlib.pyplot as plt.
+-Select row 0 of samples and assign the result to digit. For example, to select column 2 of an array a, you could use a[:,2]. Remember that since samples is a NumPy array, you can't use the .loc[] or iloc[] accessors to select specific rows or columns.
+-Print digit. This has been done for you. Notice that it is a 1D array of 0s and 1s.
+-Use the .reshape() method of digit to get a 2D array with shape (13, 8). Assign the result to bitmap.
+-Print bitmap, and notice that the 1s show the digit 7!
+-Use the plt.imshow() function to display bitmap as an image.
+'''
+# Import pyplot
+from matplotlib import pyplot as plt
+
+# Select the 0th row: digit
+digit = samples[0,:]
+
+# Print digit
+print(digit)
+
+# Reshape digit to a 13x8 array: bitmap
+bitmap = digit.reshape(13,8)
+
+# Print bitmap
+print(bitmap)
+
+# Use plt.imshow to display bitmap
+plt.imshow(bitmap, cmap='gray', interpolation='nearest')
+plt.colorbar()
+plt.show()
+
+
+
+# NMF learns the parts of images
+'''
+Now use what you've learned about NMF to decompose the digits dataset. You are again given the digit images as a 2D array samples. This time, you are also provided with a function show_as_image() that displays the image encoded by any 1D array:
+
+def show_as_image(sample):
+    bitmap = sample.reshape((13, 8))
+    plt.figure()
+    plt.imshow(bitmap, cmap='gray', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+
+After you are done, take a moment to look through the plots and notice how NMF has expressed the digit as a sum of the components!
+
+Instructions
+-Import NMF from sklearn.decomposition.
+-Create an NMF instance called model with 7 components. (7 is the number of cells in an LED display).
+-Apply the .fit_transform() method of model to samples. Assign the result to features.
+-To each component of the model (accessed via model.components_), apply the show_as_image() function to that component inside the loop.
+-Assign the row 0 of features to digit_features.
+-Print digit_features.
+'''
+# Import NMF
+from sklearn.decomposition import NMF
+
+# Create an NMF model: model
+model = NMF(n_components=7)
+
+# Apply fit_transform to samples: features
+features = model.fit_transform(samples)
+
+# Call show_as_image on each component
+for component in model.components_:
+    show_as_image(component)
+
+# Assign the 0th row of features: digit_features
+digit_features = features[0,:]
+
+# Print digit_features
+print(digit_features)
+
+
+
+# PCA doesn't learn parts
+'''
+Unlike NMF, PCA doesn't learn the parts of things. Its components do not correspond to topics (in the case of documents) or to parts of images, when trained on images. Verify this for yourself by inspecting the components of a PCA model fit to the dataset of LED digit images from the previous exercise. The images are available as a 2D array samples. Also available is a modified version of the show_as_image() function which colors a pixel red if the value is negative.
+After submitting the answer, notice that the components of PCA do not represent meaningful parts of images of LED digits!
+
+Instructions
+-Import PCA from sklearn.decomposition.
+-Create a PCA instance called model with 7 components.
+-Apply the .fit_transform() method of model to samples. Assign the result to features.
+-To each component of the model (accessed via model.components_), apply the show_as_image() function to that component inside the loop.
+'''
+# Import PCA
+from sklearn.decomposition import PCA
+
+# Create a PCA instance: model
+model = PCA(n_components=7)
+
+# Apply fit_transform to samples: features
+features = model.fit_transform(samples)
+
+# Call show_as_image on each component
+for component in model.components_:
+    show_as_image(component)
+
+
+
+# Building recommender systems using NMF
+'''
+Video
+'''
+
+
+
+# Which articles are similar to 'Cristiano Ronaldo'?
+'''
+In the video, you learned how to use NMF features and the cosine similarity to find similar articles. Apply this to your NMF model for popular Wikipedia articles, by finding the articles most similar to the article about the footballer Cristiano Ronaldo. The NMF features you obtained earlier are available as nmf_features, while titles is a list of the article titles.
+
+Instructions
+-Import normalize from sklearn.preprocessing.
+-Apply the normalize() function to nmf_features. Store the result as norm_features.
+-Create a DataFrame df from norm_features, using titles as an index.
+-Use the .loc[] accessor of df to select the row of 'Cristiano Ronaldo'. Assign the result to article.
+-Apply the .dot() method of df to article to calculate the cosine similarity of every row with article.
+-Print the result of the .nlargest() method of similarities to display the most similiar articles. This has been done for you, so hit 'Submit Answer' to see the result!
+'''
+# Perform the necessary imports
+import pandas as pd
+from sklearn.preprocessing import normalize
+
+# Normalize the NMF features: norm_features
+norm_features = normalize(nmf_features)
+
+# Create a DataFrame: df
+df = pd.DataFrame(norm_features, index=titles)
+
+# Select the row corresponding to 'Cristiano Ronaldo': article
+article = df.loc["Cristiano Ronaldo"]
+
+# Compute the dot products: similarities
+similarities = df.dot(article)
+
+# Display those with the largest cosine similarity
+print(similarities.nlargest())
+
+
+
+# Recommend musical artists part I
+'''
+In this exercise and the next, you'll use what you've learned about NMF to recommend popular music artists! You are given a sparse array artists whose rows correspond to artists and whose columns correspond to users. The entries give the number of times each artist was listened to by each user.
+In this exercise, build a pipeline and transform the array into normalized NMF features. The first step in the pipeline, MaxAbsScaler, transforms the data so that all users have the same influence on the model, regardless of how many different artists they've listened to. In the next exercise, you'll use the resulting normalized NMF features for recommendation!
+
+Instructions
+-mport:
+    -NMF from sklearn.decomposition.
+    -Normalizer and MaxAbsScaler from sklearn.preprocessing.
+    -make_pipeline from sklearn.pipeline.
+-Create an instance of MaxAbsScaler called scaler.
+-Create an NMF instance with 20 components called nmf.
+-Create an instance of Normalizer called normalizer.
+-Create a pipeline called pipeline that chains together scaler, nmf, and normalizer.
+-Apply the .fit_transform() method of pipeline to artists. Assign the result to norm_features.
+'''
--- 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/05-s&p-100-case-study.py
+++ 03-finance-fundamentals-in-python/01-introduction-to-python-for-finance/05-s&p-100-case-study.py
@@ -59,3 +59,163 @@ print(pe)
 '''
 Video
 '''
+
+
+
+# Filtering arrays
+'''
+In this lesson, you will focus on two sectors:
+    -Information Technology
+    -Consumer Staples
+numpy is imported as np and S&P 100 data is stored as arrays: names, sectors, and pe (price to earnings ratio).
+
+Instructions 1/2
+-Create a boolean array to determine which elements in sectors are 'Information Technology'.
+-Use the boolean array to subset names and pe in the Information Technology sector.
+Instructions 2/2
+-Create a boolean array to determine which elements in sectors are 'Consumer Staples'.
+-Use the boolean array to subset names and pe in the Consumer Staples sector.
+'''
+# Create boolean array 
+boolean_array = (sectors == 'Information Technology')
+
+# Subset sector-specific data
+it_names = names[boolean_array]
+it_pe = pe[boolean_array]
+
+# Display sector names
+print(it_names)
+print(it_pe)
+
+# Create boolean array 
+boolean_array = (sectors == "Consumer Staples")
+
+# Subset sector-specific data
+cs_names = names[boolean_array]
+cs_pe = pe[boolean_array]
+
+# Display sector names
+print(cs_names)
+print(cs_pe)
+
+
+
+# Summarizing sector data
+'''
+In this exercise, you will calculate the mean and standard deviation of P/E ratios for Information Technology and Consumer Staples sectors. numpy is imported as np and the it_pe and cs_pe arrays from the previous exercise are available in your workspace.
+
+Instructions 1/2
+-Calculate the mean and standard deviation of the P/E ratios (it_pe) for the Industrial Technology sector.
+Instructions 2/2
+-Calculate the mean and standard deviation of the P/E ratios (cs_pe) for the Consumer Staples sector.
+'''
+# Calculate mean and standard deviation
+it_pe_mean = np.mean(it_pe)
+it_pe_std = np.std(it_pe)
+
+print(it_pe_mean)
+print(it_pe_std)
+
+# Calculate mean and standard deviation
+cs_pe_mean = np.mean(cs_pe)
+cs_pe_std = np.std(cs_pe)
+
+print(cs_pe_mean)
+print(cs_pe_std)
+
+
+
+# Plot P/E ratios
+'''
+Let's take a closer look at the P/E ratios using a scatter plot for each company in these two sectors.
+The arrays it_pe and cs_pe from the previous exercise are available in your workspace. Also, each company name has been assigned a numeric ID contained in the arrays it_id and cs_id.
+
+Instructions
+-Draw a scatter plot of it_pe ratios with red markers and 'IT' label.
+-On the same plot, add the cs_pe ratios with green markers and 'CS' label.
+-Add a legend to this plot.
+-Display the plot.
+'''
+import matplotlib.pyplot as plt
+
+# Make a scatterplot
+plt.scatter(it_id, it_pe, color="red", label="IT")
+plt.scatter(cs_id, cs_pe, color="green", label="CS")
+
+# Add legend
+plt.legend()
+
+# Add labels
+plt.xlabel('Company ID')
+plt.ylabel('P/E Ratio')
+plt.show()
+
+
+
+# Visualizing trends
+'''
+Video
+'''
+
+
+
+# Histogram of P/E ratios
+'''
+To visualize and understand the distribution of the P/E ratios in the IT sector, you can use a histogram.
+The array it_pe from the previous exercise is available in your workspace.
+
+Instructions
+-Selectively import the pyplot module of matplotlib as plt.
+-Plot a histogram of it_pe with 8 bins.
+-Add the x-label as 'P/E ratio' and y-label as 'Frequency'.
+-Display the plot.
+'''
+# Import matplotlib.pyplot with the alias plt
+import matplotlib.pyplot as plt
+
+# Plot histogram 
+plt.hist(it_pe, bins=8)
+
+# Add x-label
+plt.xlabel('P/E ratio')
+
+# Add y-label
+plt.ylabel('Frequency')
+
+# Show plot
+plt.show()
+
+
+
+# Identify the outlier
+'''
+Histograms can help you to identify outliers or abnormal data points. Which P/E ratio in this histogram is an example of an outlier?
+
+Possible Answers
+-A stock with P/E ratio < 20.
+-A stock with 20 < P/E ratio < 30.
+-A stock with 30 < P/E ratio < 40.
+-A stock with P/E ratio > 50.
+
+# A stock with P/E ratio > 50.
+'''
+
+
+
+# Name the outlier
+'''
+You've identified that a company in the Industrial Technology sector has a P/E ratio of greater than 50. Let's identify this company.
+numpy is imported as np, and arrays it_pe (P/E ratios of Industrial Technology companies) and it_names (names of Industrial Technology companies) are available in your workspace.
+
+Instructions
+-Identify the P/E ratio greater than 50 and assign it to outlier_price.
+-Identify the company with P/E ratio greater than 50 and assign it to outlier_name.
+'''
+# Identify P/E ratio within it_pe that is > 50
+outlier_price = it_pe[ it_pe > 50]
+
+# Identify the company with PE ratio > 50
+outlier_name = it_names[it_pe == outlier_price]
+
+# Display results
+print("In 2017, " + str(outlier_name[0]) + " had an abnormally high P/E ratio of " + str(round(outlier_price[0], 2)) + ".")
