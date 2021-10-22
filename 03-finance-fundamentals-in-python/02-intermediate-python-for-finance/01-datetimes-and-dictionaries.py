80c0337f37ebc569ef5495562d6a262484077fb5
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-22 17:29:33 -0500

    Create 01-datetimes-and-dictionaries.py

---
+++ 03-finance-fundamentals-in-python/02-intermediate-python-for-finance/01-datetimes-and-dictionaries.py
@@ -0,0 +1,18 @@
+# 01 Datetimes and Dictionaries
+'''
+In this chapter, you’ll learn how to create and manipulate Python datetime objects to help you identify key financial events, such as Black Friday. You’ll also learn how to store and efficiently look up items using Python dictionaries.
+'''
+
+
+
+# Representing time with datetimes
+'''
+Video
+'''
+
+
+
+# Creating datetimes for dates
+'''
+
+'''
