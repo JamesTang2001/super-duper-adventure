3335737385579cef9af325ffab5686f35cf339dc
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-22 17:23:34 -0500

    Create 01-applying-logistic-regression-and-svm.py

---
+++ 01-machine-learning-fundamentals-with-python/03-linear-classifiers-in-python/01-applying-logistic-regression-and-svm.py
@@ -0,0 +1,18 @@
+# 01 Applying logistic regression and SVM
+'''
+In this chapter you will learn the basics of applying logistic regression and support vector machines (SVMs) to classification problems. You'll use the scikit-learn library to fit classification models to real data.
+'''
+
+
+
+# scikit-learn refresher
+'''
+Video
+'''
+
+
+
+#
+'''
+
+'''
