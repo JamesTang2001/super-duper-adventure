901212a11e0bd72162da15585c965d1c9adeda57
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-23 17:45:34 -0500

    Create 02-control-flow-and-logic.py

---
+++ 03-finance-fundamentals-in-python/02-intermediate-python-for-finance/02-control-flow-and-logic.py
@@ -0,0 +1,18 @@
+# 02 Control Flow and Logic
+'''
+Through hands-on activities, you’ll discover how to use Boolean logic to determine truth and use comparison and equality operators to control execution in if-statements and loops.
+'''
+
+
+
+# Comparison operators
+'''
+Video
+'''
+
+
+
+# 
+'''
+
+''''
