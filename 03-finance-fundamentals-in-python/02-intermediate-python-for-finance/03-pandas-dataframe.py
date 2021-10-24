a3770d56e22bee6181e434dfc962ef661481ae52
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-24 12:20:51 -0500

    Create 03-pandas-dataframe.py

---
+++ 03-finance-fundamentals-in-python/02-intermediate-python-for-finance/03-pandas-dataframe.py
@@ -0,0 +1,18 @@
+# 03 Pandas Dataframe
+'''
+Discover how to create and access DataFrames with pandas using financial data from other data structures, including Dicts, lists, and CSV files. You’ll then uncover additional insights, as you aggregate data across rows or columns, calculate averages, and extend your data using functions.
+'''
+
+
+
+# Creating a DataFrame
+'''
+Video
+'''
+
+
+
+# 
+'''
+
+'''
