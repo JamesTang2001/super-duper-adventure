f811c140bd1b2453d49e6f7809e89fed206c0292
James Tang <51824640+JamesTang2001@users.noreply.github.com>
2021-10-24 11:37:17 -0500

    Create 03-logistic-regression.py

---
+++ 01-machine-learning-fundamentals-with-python/03-linear-classifiers-in-python/03-logistic-regression.py
@@ -0,0 +1,18 @@
+# 03 Logistic regression
+'''
+In this chapter you will delve into the details of logistic regression. You'll learn all about regularization and how to interpret model output.
+'''
+
+
+
+# Logistic regression and regularization
+'''
+Video
+'''
+
+
+
+# 
+'''
+
+'''
