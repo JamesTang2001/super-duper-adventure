152aa1d6d87ec3ca176dd18891c7b7ec79245041
JamesTang <jamestanggood@gmail.com>
2021-10-23 20:51:03 -0500

    Complete chapter 02 Loss functions.

--- 01-machine-learning-fundamentals-with-python/03-linear-classifiers-in-python/02-loss-functions.py
+++ 01-machine-learning-fundamentals-with-python/03-linear-classifiers-in-python/02-loss-functions.py
@@ -12,7 +12,162 @@ Video
 
 
 
-# 
+# How models make predictions
+'''
+Which classifiers make predictions based on the sign (positive or negative) of the raw model output?
+
+Possible Answers
+-Logistic regression only
+-Linear SVMs only
+-Neither
+-Both logistic regression and Linear SVMs
+
+# Both logistic regression and Linear SVMs
 '''
 
+
+
+# Changing the model coefficients
 '''
+When you call fit with scikit-learn, the logistic regression coefficients are automatically learned from your dataset. In this exercise you will explore how the decision boundary is represented by the coefficients. To do so, you will change the coefficients manually (instead of with fit), and visualize the resulting classifiers.
+A 2D dataset is already loaded into the environment as X and y, along with a linear classifier object model.
+
+Instructions
+-Set the two coefficients and the intercept to various values and observe the resulting decision boundaries.
+-Try to build up a sense of how the coefficients relate to the decision boundary.
+-Set the coefficients and intercept such that the model makes no errors on the given training data.
+'''
+# Set the coefficients
+model.coef_ = np.array([[0,1]])
+model.intercept_ = np.array([0])
+
+# Plot the data and decision boundary
+plot_classifier(X,y,model)
+
+# Print the number of errors
+num_err = np.sum(y != model.predict(X))
+print("Number of errors:", num_err)
+
+
+
+# What is a loss function?
+'''
+Video
+'''
+
+
+
+# The 0-1 loss
+'''
+In the figure below, what is the 0-1 loss (number of classification errors) of the classifier?
+
+Possible Answers
+-0
+-1
+-2
+-3
+
+# 2
+'''
+
+
+
+# Minimizing a loss function
+'''
+In this exercise you'll implement linear regression "from scratch" using scipy.optimize.minimize.
+We'll train a model on the Boston housing price data set, which is already loaded into the variables X and y. For simplicity, we won't include an intercept in our regression model.
+
+Instructions
+-Fill in the loss function for least squares linear regression.
+-Print out the coefficients from fitting sklearn's LinearRegression.
+'''
+# The squared error, summed over training examples
+def my_loss(w):
+    s = 0
+    for i in range(y.size):
+        # Get the true and predicted target values for example 'i'
+        y_i_true = y[i]
+        y_i_pred = w@X[i]
+        s = s + (y_i_true - y_i_pred)**2
+    return s
+
+# Returns the w that makes my_loss(w) smallest
+w_fit = minimize(my_loss, X[0]).x
+print(w_fit)
+
+# Compare with scikit-learn's LinearRegression coefficients
+lr = LinearRegression(fit_intercept=False).fit(X,y)
+print(lr.coef_)
+
+
+
+# Loss function diagrams
+'''
+Video
+'''
+
+
+
+# Classification loss functions
+'''
+Which of the four loss functions makes sense for classification?
+
+Possible Answers
+-(1)
+-(2)
+-(3)
+-(4)
+
+# (2)
+'''
+
+
+
+# Comparing the logistic and hinge losses
+'''
+In this exercise you'll create a plot of the logistic and hinge losses using their mathematical expressions, which are provided to you.
+The loss function diagram from the video is shown on the right.
+
+Instructions
+-Evaluate the log_loss() and hinge_loss() functions at the grid points so that they are plotted.
+'''
+# Mathematical functions for logistic and hinge losses
+def log_loss(raw_model_output):
+   return np.log(1+np.exp(-raw_model_output))
+def hinge_loss(raw_model_output):
+   return np.maximum(0,1-raw_model_output)
+
+# Create a grid of values and plot
+grid = np.linspace(-2,2,1000)
+plt.plot(grid, log_loss(grid), label='logistic')
+plt.plot(grid, hinge_loss(grid), label='hinge')
+plt.legend()
+plt.show()
+
+
+
+# Implementing logistic regression
+'''
+This is very similar to the earlier exercise where you implemented linear regression "from scratch" using scipy.optimize.minimize. However, this time we'll minimize the logistic loss and compare with scikit-learn's LogisticRegression (we've set C to a large value to disable regularization; more on this in Chapter 3!).
+The log_loss() function from the previous exercise is already defined in your environment, and the sklearn breast cancer prediction dataset (first 10 features, standardized) is loaded into the variables X and y.
+
+Instructions
+-Input the number of training examples into range().
+-Fill in the loss function for logistic regression.
+-Compare the coefficients to sklearn's LogisticRegression.
+'''
+# The logistic loss, summed over training examples
+def my_loss(w):
+    s = 0
+    for i in range(y.size):
+        raw_model_output = w@X[i]
+        s = s + log_loss(raw_model_output * y[i])
+    return s
+
+# Returns the w that makes my_loss(w) smallest
+w_fit = minimize(my_loss, X[0]).x
+print(w_fit)
+
+# Compare with scikit-learn's LogisticRegression
+lr = LogisticRegression(fit_intercept=False, C=1000000).fit(X,y)
+print(lr.coef_)
--- 03-finance-fundamentals-in-python/02-intermediate-python-for-finance/02-control-flow-and-logic.py
+++ 03-finance-fundamentals-in-python/02-intermediate-python-for-finance/02-control-flow-and-logic.py
@@ -12,7 +12,126 @@ Video
 
 
 
-# 
+# Equality across types
+'''
+The equality operator == can be used to test if two variables have the same value. What is the result of using the equality operator to compare a variable closing_price, which holds the floating point value for a stocks closing price, to the variable closing_time, which holds a datetime object with the time of closing as a value.
+
+Possible Answers
+-True
+-False
+-A TypeError complaining about comparing different types
+-A warning message complaining about comparing different types
+
+# False
 '''
 
-''''
+
+
+# Assignment and equality
+'''
+When looking at someone's wealth, a distinction between cash and non-cash securities is important. Savings and checking accounts are cash securities. Stock positions are non-cash. Imagine that as part of balancing a client's wealth portfolio, you are asked to compare their cash and non-cash securities. The variable non_cash is provided with the value of the non-cash securities.
+
+Instructions
+-Assign a value of 19.11 to the variable cash.
+-Check if cash and non_cash have the same value.
+-Assign to cash the value of non_cash.
+-Confirm that cash and non_cash are equal.
+'''
+# Assign a value to cash.
+cash = 19.11
+
+# Check if cash is equal to non-cash
+print(cash == non_cash)
+
+# Assign the value of cash to be equal non-cash
+cash = non_cash
+
+# Check if cash is equal to non-cash
+print(cash == non_cash)
+
+
+
+# Comparing dividends
+'''
+Dividends are payments made to stockholders, usually from profits that are not re-invested in a company. It is important to identify which stocks are paying higher dividends when analyzing how to balance stocks in a portfolio. Let's say that in your role analyzing a client's portfolio, you are tasked with comparing dividends from different holdings. The values of these dividends are provided in the variables d1 and d2.
+
+Instructions 1/2
+-Find out if dividend d2 is greater than zero.
+-See if dividend d1 is greater than d2.
+Instructions 2/2
+-Check that dividend 1 is at least 100 units.
+-Check that dividend 1 is not more than dividend 2.
+'''
+# Check dividend is greater than zero
+print(d2 > 0)
+
+# Is dividend 1 is greater than dividend 2?
+print(d1 > d2)
+
+# Check dividend 1 is at least 100
+print(d1 >= 100)
+
+# Check dividend 2 is at least as much dividend 1
+print(d1 <= d2)
+
+
+
+# Boolean operators
+'''
+Video
+'''
+
+
+
+# Decisions with Boolean operations
+'''
+Boolean operations are used in Python to make decisions. Imagine that you work as an analyst identifying accounts that might potentially participate in stock trades. You might use this information to advise qualifying clients on market moves. The variable is_investment_account is supplied. It is set to True if the given account includes stock positions as part of it's holdings. The supplied variable balance_positive is set to True if the account has a positive cash balance, which could be used to purchase securities.
+
+Instructions
+-Print the value of the variable balance_positive.
+-Set the variable potential_trade to True if the account is an investment account with a positive balance.
+-Print the True if this is a potential trade.
+'''
+# Print the given variables
+print(is_investment_account)
+print(balance_positive)
+
+# Decide if this account is cantidate for trading advice
+potential_trade = is_investment_account and balance_positive
+
+# Print if this represents a potential trade
+print(potential_trade)
+
+
+
+# Assigning variables with Boolean operators
+'''
+You can use the fact that and and or are short-circuit operators to assign objects to variables in intelligent ways. Pretend that you are deciding what actions you should take with a client's account based on what they entered in a web-form. Unfortunately, they could submit the form with this field empty, so you need to set a default action just in case. The supplied variable input_action has the contents submitted by the client. The supplied variable is_trading_day is True if today is a day that trades are possible.
+
+Instructions
+-If your client entered an action, assign it to the variable action. If they entered nothing, use the default action "Hold".
+-Assign the action to the variable do_action if trades can be done today, otherwise assign it as False.
+-Print the action that should be done.
+'''
+# Assign a default action if no input
+action = input_action or "Hold"
+
+# Print the action
+print(action)
+
+# Assign action only if trades can be made
+do_action = is_trading_day and action
+
+# Print the action to do
+print(do_action)
+
+
+
+# Negating with Boolean operators
+'''
+It is often the case that you need to combine Boolean operations to solve more complicated problems. Let's say that you need to know if you need to download the closing stock prices for today. You only want to do the operation if the markets have already closed. The closing prices are a list held in the provided variable closing_prices. The supplied variable market_closed is set to True once the markets close. Use Boolean operations and what you know about how objects evaluate in them to determine if you should download the data.
+
+Instructions
+-Set the variable not_prices to True if you do not have closing prices for today.
+-Set the variable get_prices to True if the market is closed and you don't have closing prices yet.
+'''
